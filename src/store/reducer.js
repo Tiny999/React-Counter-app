a3fe561243f280da2cc648c93af999bf9040db81
Tiny999 <salamiabbas3@gmail.com>
2020-09-23 21:37:49 +0100

    first-commit

---
+++ src/store/reducer.js
@@ -0,0 +1,46 @@
+
+const initialState = {
+  counter: 0,
+  results: []
+}
+
+const reducer = (state = initialState, action) => {
+  if(action.type === 'INCREMENT'){
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  }
+
+  if(action.type === 'DECREMENT'){
+    return {
+      ...state,
+      counter: state.counter - 1
+    }
+  }
+
+  if(action.type === 'ADD'){
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
+
+  if(action.type === 'SUBTRACT'){
+    return {
+      ...state,
+      counter: state.counter - action.value
+    }
+  }
+
+  if(action.type === 'STORE_RESULT'){
+    return {
+      ...state,
+      results: state.results.concat({id: new Date(),value: state.counter})
+    }
+  }
+
+  return state;
+}
+
+export default reducer;
